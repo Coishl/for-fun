8b144cf3593e4f51261cff37964ea06f02bc8f09
Coishl <jakarishaw27@gmail.com>
2024-08-24 12:15:39 -0500

    Create madlib.py

---
+++ madlib.py
@@ -0,0 +1,9 @@
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+adjective = input("Enter a adjective: ")
+place = input("Enter a place: ")
+
+story = f"Once upon a {place} there was a {adjective} {noun} who loved to {verb}"
+
+print("/nHere is your story: ")
+print(story)
